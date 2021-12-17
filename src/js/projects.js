043fa95cf637e1e9d5f863997f82969675a182e4
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-12-17 23:05:56 +0200

    added mobile version

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,5 +1,11 @@
 import tpl from '../partials/templates/projects.hbs';
 import { teamProjects, ownProjects } from './projects-db';
+import { addBackToTop } from 'vanilla-back-to-top';
+addBackToTop({
+  diameter: 56,
+  backgroundColor: '#1e2939',
+  textColor: '#fff',
+});
 
 const refs = {
   team: document.querySelector('.js-team'),
